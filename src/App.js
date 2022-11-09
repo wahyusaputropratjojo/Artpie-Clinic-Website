73542cab18c08918f53e0e4e5f8dc245b49533bd
Wahyu Saputro Pratjojo <wahyupratjojo.opos@gmail.com>
2022-11-09 20:10:08 +0800

    Memperbarui Header, Membuat Halaman Baru dan Mengimplementasi Router
    Mengubah warna header untuk Mobile. Menambah beberapa halaman baru diantaranya Beranda, Layanan, Lokasi dan Tentang. Mengimplementasi Router ke App.js serta Navigasi pada Header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
+import Beranda from './pages/Beranda';
+import Layanan from './pages/Layanan';
+import Lokasi from './pages/Lokasi';
+import Tentang from './pages/Tentang';
 
 const App = () => {
 	return (
 		<>
 			<Header />
+			<Routes>
+				<Route path="/" element={<Beranda />} />
+				<Route path="/layanan" element={<Layanan />} />
+				<Route path="/lokasi" element={<Lokasi />} />
+				<Route path="/tentang" element={<Tentang />} />
+			</Routes>
 		</>
 	);
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ReactComponent as ArtpieClinicLogoRemake } from '../assets/artpie-clinic-logo-remake.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	const [navbar, setNavbar] = useState(false);
@@ -13,6 +14,14 @@ const Header = () => {
 
 	const handleNavbar = () => {
 		setNavbar((p) => !p);
+	};
+
+	const handleNavbarMobile = () => {
+		if (navbar === false) {
+			setNavbar(false);
+		} else if (navbar === true) {
+			setNavbar(false);
+		}
 	};
 
 	useEffect(() => {
@@ -36,20 +45,44 @@ const Header = () => {
 		<>
 			<header>
 				<div className="px-10 py-6 lg:px-0 mx-auto my-0 max-w-screen-lg flex justify-between items-center">
-					<ArtpieClinicLogoRemake className="w-[4rem] md:w-[5rem] transition-all" />
+					<ArtpieClinicLogoRemake className="w-[4rem] mx-auto md:mx-0 md:w-[5rem] transition-all" />
 					<nav>
 						<ul className={navbar ? 'mobile-navbar' : 'desktop-navbar'}>
-							<li>Beranda</li>
-							<li>Tentang</li>
-							<li>Layanan</li>
-							<li>Lokasi</li>
+							<li>
+								<Link onClick={handleNavbarMobile} to="/">
+									Beranda
+								</Link>
+							</li>
+							<li>
+								<Link onClick={handleNavbarMobile} to="/layanan">
+									Layanan
+								</Link>
+							</li>
+							<li>
+								<Link onClick={handleNavbarMobile} to="/lokasi">
+									Lokasi
+								</Link>
+							</li>
+							<li>
+								<Link onClick={handleNavbarMobile} to="/tentang">
+									Tentang
+								</Link>
+							</li>
 						</ul>
 					</nav>
-					<FontAwesomeIcon
-						onClick={handleNavbar}
-						className="h-10 md:hidden z-10 transition-all"
-						icon={navbar ? faXmark : faBars}
-					/>
+					{navbar ? (
+						<FontAwesomeIcon
+							onClick={handleNavbar}
+							className="h-10 md:hidden z-10 transition-all text-white"
+							icon={faXmark}
+						/>
+					) : (
+						<FontAwesomeIcon
+							onClick={handleNavbar}
+							className="h-10 md:hidden z-10 transition-all"
+							icon={faBars}
+						/>
+					)}
 				</div>
 			</header>
 		</>
