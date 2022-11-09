ce97b877f4604d9410b822f1709cd4b8ad9abc42
Wahyu Saputro Pratjojo <wahyupratjojo.opos@gmail.com>
2022-11-09 20:25:39 +0800

    Membuat Hero Section untuk halaman Beranda

--- src/pages/Beranda/index.js
+++ src/pages/Beranda/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import HeroSection from './HeroSection';
 
 const Beranda = () => {
 	return (
 		<>
-			<div>Home</div>
+			<main>
+				<HeroSection />
+			</main>
 		</>
 	);
 };
